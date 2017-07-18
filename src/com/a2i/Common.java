7ddbc8297b65916c9ee9c0c4faa00cc4039b942f
sclardy75 <stephen.clardy@archarithms.com>
2017-07-18 16:10:28 -0500

    p001 solution

---
+++ src/com/a2i/Common.java
@@ -0,0 +1,46 @@
+package com.a2i;
+
+import static java.lang.Math.abs;
+
+/*******************************************************************************
+ *  Copyright (c) 2017, Archarithms, Inc.
+ *
+ *  Concept developed in: Huntsville, AL
+ *  Design developed in:  Huntsville, AL 
+ *  Produced in:          Huntsville, AL
+ *******************************************************************************/
+class Common
+{
+  // return true if value n is a palindrome (reads the same backward or forward)
+  static boolean isPalindrome(long n) {
+    long rev = 0;
+    long num = n;
+    while (num > 0) {
+      rev = rev * 10 + num % 10;
+      num /= 10;
+    }
+    return rev == n;
+  }
+
+  // return greatest common divisor of m and n
+  static long gcd(long m, long n) {
+    while(m != 0) {
+      long m_old = m;
+      m = n % m;
+      n = m_old;
+    }
+    return abs(n);
+  }
+
+
+  // recursive version
+  // return greatest common divisor of m ans n
+  static long gcd2(long m, long n) {
+    if (m == 0) {
+      return abs(n);
+    } else {
+      return gcd2(n % m, m);
+    }
+  }
+
+}
---
+++ src/com/a2i/P001.java
@@ -0,0 +1,67 @@
+package com.a2i;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+/*******************************************************************************
+ *  Copyright (c) 2017, Archarithms, Inc.
+ *
+ *  Concept developed in: Huntsville, AL
+ *  Design developed in:  Huntsville, AL
+ *  Produced in:          Huntsville, AL
+ *******************************************************************************/
+
+class P001
+{
+
+    public static void main(String[] args) {
+      System.out.println("1001 th prime = " + Primes.nthPrime(1001));
+
+      boolean[] primes = Primes.getPrimes(25);
+      System.out.println(Arrays.toString(primes));
+
+      System.out.println("is 13 prime = " + primes[13]);
+
+      System.out.println("isPalindrome(1234321 = " + Common.isPalindrome(1234321));
+      System.out.println("isPalindrome(1244321 = " + Common.isPalindrome(1244321));
+
+      System.out.println("gcd(144, 72) == gcd(144, 72) = " + (Common.gcd(144, 72) == Common.gcd2(144, 72)));
+
+      System.out.println("gcd(144, 72) =" + Common.gcd(144, 72));
+
+      System.out.println("euler 1 test case solve = " + solve(10));
+      System.out.println("euler 1 solve = " + solve(1000));
+      System.out.println("euler 1 solveStream = " + solveStream(1000));
+      System.out.println("euler 1 solveStream2 = " + solveStream2(1000));
+
+
+    }
+
+    private static Integer solve(Integer n) {
+
+      Integer sum = 0;
+      for (int i = 1; i < n; i++)
+      {
+        if(i % 3 == 0 || i % 5 == 0) {
+          sum += i;
+        }
+      }
+      return sum;
+    }
+
+  private static Integer solveStream(Integer n) {
+
+    Integer res =  IntStream
+        .range(3, 1000)
+        .filter(i -> i % 3 == 0 || i % 5 == 0)
+        .sum();
+
+    return res;
+  }
+
+  private static Integer solveStream2(Integer n) {
+
+     return IntStream.range(3, 1000).filter(i -> i % 3 == 0 || i % 5 == 0).sum();
+
+  }
+
+}
